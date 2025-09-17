4d9164e4d3870c7fef2519b8b0dfd4576add3d81
Lamine NDIAYE <lamine12012004@gmail.com>
2025-09-17 16:06:56 +0000

    partie backend

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -5,7 +5,7 @@ const Model=require('../models/model');
 let tableauDemandes = [];
 let idCounter = 1;
 
-router.post('/', (req, res) => {
+router.post('/', (req, res) => {             // route post me permet de creer une demande
   console.log("Body reçu :", req.body); // <-- ça va afficher dans le terminal ce que Postman envoie
 
   const { prenom, nom, sexe, handicap, type } = req.body;
@@ -28,8 +28,22 @@ router.post('/', (req, res) => {
   res.status(201).json(nouvelleDemande);
 });
 
-router.get('/', (req, res) => {
+router.get('/', (req, res) => {        //route get me permet de recuperer les demandes
   res.json(tableauDemandes);
 });
+
+router.get('/:id',(req,res)=>{   //cette route nous permet de recuperer une demande par son id
+  const recupererId=parseInt(req.params.id);
+  const chercherId=tableauDemandes.find(index=>index.id===id);
+  if(chercherId){
+    res.json(chercherId);
+  }else{
+    res.status(404).json({message:"Cette id n'existe pas"});
+  }
+});
+
+router.put('/:id',(req,res)=>{
+  
+})
 
 module.exports = router;
